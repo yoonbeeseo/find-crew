a9f63c98d6546377011e41c976012fb417bdd902
yoonbeeseo <yoon.tec.info@gmail.com>
2025-03-31 16:54:11 +0900

    deploy

---
+++ dist/assets/AppForm-Cm6-aqIp.js
@@ -0,0 +1,1 @@
+import{j as a,t as e}from"./index-BN9P0I-l.js";const u=t=>a.jsx("form",{...t,onSubmit:m=>{m.preventDefault(),t.onSubmit&&t.onSubmit(m)},className:e("max-w-100 mx-auto col gap-y-2.5 p-5",t==null?void 0:t.className)});export{u as A};
